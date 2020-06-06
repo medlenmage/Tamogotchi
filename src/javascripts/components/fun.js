3cd13ac30aad5e58caf8821e557b96a4ed5e77bb
Joshua Medlen <joshua246@gmail.com>
2020-06-06 09:27:29 -0500

    added play buttons still needs styling

---
+++ src/javascripts/components/fun.js
@@ -0,0 +1,32 @@
+import printToDom from '../helpers/utils';
+
+let fun = 50;
+
+const tamoFun = () => {
+  let domString = '';
+  domString += `
+    <div id="counter">${fun}</div>
+    <button class="real-fun-btn" id="real-fun-counter">Real Fun</button>
+    <button class="sorta-fun-btn" id="sorta-fun-counter">Sorta Fun</button>
+  `;
+
+  printToDom.printToDom('#play', domString);
+};
+
+const realFun = () => {
+  if (fun === 100) return;
+  fun += 10;
+  if (fun > 100) fun = 100;
+  tamoFun();
+};
+
+const tamoKindaFun = () => {
+  if (fun === 100) return;
+  fun += 2;
+  if (fun > 0) fun = 100;
+  tamoFun();
+};
+$('body').on('click', '#real-fun-counter', realFun);
+$('body').on('click', '#sorta-fun-counter', tamoKindaFun);
+
+export default { tamoFun };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,10 +1,12 @@
 import 'bootstrap';
 import '../styles/main.scss';
 import eatDom from './components/eat';
+import tamoFun from './components/fun';
 
 
 const init = () => {
   eatDom.tamoEat();
+  tamoFun.tamoFun();
 };
 
 init();
