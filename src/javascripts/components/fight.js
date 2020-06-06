6c34eabbd0cbf38875ce2f4bca3c5d5cd552fbd0
Joshua Medlen <joshua246@gmail.com>
2020-06-06 09:38:57 -0500

    added fight module and buttons. still needs styling

---
+++ src/javascripts/components/fight.js
@@ -0,0 +1,31 @@
+import printToDom from '../helpers/utils';
+
+let strength = 100;
+
+const tamoFight = () => {
+  let domString = '';
+  domString += `
+    <h3>Fighting</h3>
+    <div id="counter">${strength}</div>
+    <button class="run-away-btn" id="run-counter">Run Away</button>
+    <button class="violence-btn" id="violence-counter">Get Violent</button>
+  `;
+
+  printToDom.printToDom('#fight', domString);
+};
+
+const runAway = () => {
+  strength += 1;
+  tamoFight();
+};
+
+const squareUp = () => {
+  if (strength === 0) return;
+  strength -= 10;
+  if (strength < 0) strength = 0;
+  tamoFight();
+};
+$('body').on('click', '#run-counter', runAway);
+$('body').on('click', '#violence-counter', squareUp);
+
+export default { tamoFight };
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,11 +2,13 @@ import 'bootstrap';
 import '../styles/main.scss';
 import eatDom from './components/eat';
 import tamoFun from './components/fun';
+import squareUp from './components/fight';
 
 
 const init = () => {
   eatDom.tamoEat();
   tamoFun.tamoFun();
+  squareUp.tamoFight();
 };
 
 init();
